8da126d4ade87d190919ab823544af3143ac658e
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-23 11:05:13 +0100

    add changes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
- * Initialization data for the jupyterlab-iframe-bridget-example extension.
+ * Initialization data for the jupyterlab-iframe-bridge-example extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-iframe-bridget-example:plugin',
-  description: 'Communication between a host page and an instance of JupyterLab',
+  id: 'jupyterlab-iframe-bridge-example:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab-iframe-bridget-example is activated!');
-  }
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, themeManager: IThemeManager) => {
+    console.log('JupyterLab extension jupyterlab-iframe-bridge-example is activated!');
+
+    /* Incoming messages management */
+    window.addEventListener('message', (event) => {
+      if (event.data.type === 'from-host-to-iframe') {
+        console.log('Message received in the iframe:', event.data);
+
+        if (themeManager.theme === 'JupyterLab Dark') {
+          themeManager.setTheme('JupyterLab Light');
+        } else {
+          themeManager.setTheme('JupyterLab Dark');
+        }
+      }
+    });
+
+    /* Outgoing messages management */
+    const notifyThemeChanged = (): void => {
+      const message = { type: 'from-iframe-to-host', theme: themeManager.theme };
+      window.parent.postMessage(message, '*');
+      console.log('Message sent to the host:', message);
+    };
+    themeManager.themeChanged.connect(notifyThemeChanged);
+  },
 };
 
 export default plugin;
